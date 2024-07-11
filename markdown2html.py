5205b4aaddcd5e29669d4374f9ff0844cc2dd8b4
Richouf95 <wourichouf95@gmail.com>
2024-07-11 16:07:54 +0100

    Fix << Start a script >>

--- markdown2html.py
+++ markdown2html.py
@@ -11,18 +11,20 @@ def markdown_to_html():
     Transform markdown file to html
     """
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
     markdown_file_name = sys.argv[1]
     html_file_name = sys.argv[2]
 
     if not os.path.exists(markdown_file_name):
-        print("Missing <filename>")
+        sys.stderr.write("Missing {}\n".format(markdown_file_name))
         sys.exit(1)
 
-    print("arg 1 : {}".format(markdown_file_name))
-    print("arg 2 : {}".format(html_file_name))
+    # Here you would implement the conversion from markdown to HTML
+    # For now, we'll just print that the conversion would take place
+    # print("arg 1 : {}".format(markdown_file_name))
+    # print("arg 2 : {}".format(html_file_name))
 
 if __name__ == "__main__":
     markdown_to_html()
